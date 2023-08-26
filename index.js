dfa634077050872964b88ef118ec829db31d2fdc
Stevenzhunk <kikexgaz@gmail.com>
2023-08-26 00:20:53 -0300

    fix responsive and navfull

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const mobileMenu = document.getElementById("mobile-menu");
 const navList = document.getElementById("nav-list");
+const navOn = document.getElementById("nav-active");
 
 mobileMenu.addEventListener("click", () => {
   navList.classList.toggle("active");
   mobileMenu.classList.toggle("menu-open"); // Agrega o quita la clase menu-open
+  navOn.classList.toggle("navfull");
 });
